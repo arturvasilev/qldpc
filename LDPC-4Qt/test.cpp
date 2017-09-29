990c8e253e71dad642429396d4866a5c0a6feb7b
Dmitriy Osadchiy <frozenseller@gmail.com>
2017-09-29 22:24:43 +0300

    Initial commit

---
+++ LDPC-4Qt/test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "ldpc4qt.h"
+
+int main(){
+    return 0;
+}
---
+++ testLDPC/main.cpp
@@ -0,0 +1,39 @@
+//#include <QCoreApplication>
+#include <QtWidgets/QApplication>
+#include "../libLDPC/libldpc.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    cout<<"kek"<<endl;
+    vector<bool> check, ldpc, ans;
+
+    for(int i = 0 ; i < 1000; i++)
+    {
+        check.push_back(rand()%2);
+        cout<<check.at(i)<<' ';
+    }
+    cout<<endl<<endl;
+
+    ldpc = Encoder(check, 0.035, 3);
+
+    for(int i = 0 ; i < ldpc.size(); i++)
+    {
+        cout<<ldpc.at(i)<<' ';
+    }
+    cout<<endl<<endl;
+
+    ans = Decoder(check, ldpc, 0.035,"q", 3);
+
+    for(int i = 0 ; i <ans.size(); i++)
+    {
+        cout<<ans.at(i)<<' ';
+    }
+    cout<<endl;
+    a.exec();
+    return 0;
+}
